3cf40438198660791c4c29ae3c2cb13d222dc194
Macy Kay Mannix <macymannix@gmail.com>
2022-05-01 15:21:27 -0700

    js for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,46 @@
 var apiKey = "aecaeed4eefe593c25f498d7c5899d1c";
-var apiCall = "api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apiKey;
-var lat ;
-var lon  ;
 var searchFormEl = document.getElementById('search-bar');
 var searchInputEl = document.getElementById('city-input');
 var searchBtnEl = document.getElementById('search-btn');
 var searchHistoryEl = document.getElementById('recent-search');
 // var favesEl = document.getElementById('current-weather');
+
+//call city first as function this workssss yassss bless Hector
+function getApi () {
+    var geoApi = "http://api.openweathermap.org/geo/1.0/direct?q="+searchInputEl.value+"&appid="+apiKey;
+    fetch(geoApi)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    getApi2((data[0].lat), (data[0].lon))
+});
+}
+
+
+function getApi2(lat, lon) {
+   var apiCall = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=imperial&appid="+apiKey;
+    fetch(apiCall)
+   .then(function(response) {
+     return response.json();
+   })
+   .then(function(data) {
+    console.log(data);
+
+    //  for (var i = 0; i < data.length; i++) {
+    //    var listEl = document.createElement('li');
+    //    listEl.innerHTML = "<div >"
+    //    listEl.textContent = searchInputEl.value;
+    //    searchHistoryEl.appendChild(listEl);
+     
+   });
+}
+
+searchBtnEl.addEventListener('click', getApi);
+
+
+
 
 
 searchBtnEl.addEventListener('click', function(event) {
@@ -15,9 +49,13 @@ searchBtnEl.addEventListener('click', function(event) {
    console.log(searchInputEl.value);
   
    
-    var listEl = document.createElement('li');
-    listEl.innerHTML = "<div >"
-    listEl.textContent = searchInputEl.value;
-    searchHistoryEl.appendChild(listEl);
+    // var listEl = document.createElement('li');
+    // listEl.innerHTML = "<div >"
+    // listEl.textContent = searchInputEl.value;
+    // searchHistoryEl.appendChild(listEl);
     
 });
+
+
+var fetchButton = document.getElementById('search-button');
+
